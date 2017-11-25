d4c09e018da1ad3b24994e80935726f8a7aa124d
Jesus Vallejo <jesus.vallejo.collados@gmail.com>
2017-11-25 14:14:06 +0100

    Update esp8266-mqtt-matrix.ino

--- esp8266-mqtt-matrix.ino
+++ esp8266-mqtt-matrix.ino
@@ -174,9 +174,6 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
  
 void loop() {
-  if (WiFi.status() != WL_CONNECTED){
-  WiFi.begin(ssid, password);
-  }
   if (!client.connected()) {
         reconnect();
   }
